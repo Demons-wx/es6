efc19a2a17bd1fd8d812b4665fe1a0ea38dc633e
wangxuan <demons_wx@163.com>
2018-09-27 23:15:13 +0800

    模块化和Decorator

--- app/js/index.js
+++ app/js/index.js
@@ -2,7 +2,19 @@
 * @Author: wangxuan
 * @Date:   2018-09-22 15:27:15
 * @Last Modified by:   wangxuan
-* @Last Modified time: 2018-09-24 22:00:20
+* @Last Modified time: 2018-09-27 23:13:21
 */
 // import 'babel-polyfill';
-import './class/lesson15';
+// import './class/lesson17';
+
+// import {A, test, Hello} from './class/lesson17';
+// console.log(A, test, Hello);
+
+// import {A} from './class/lesson17';
+// console.log(A);
+
+// import * as lesson from './class/lesson17';
+// console.log(lesson.A, lesson.test);
+
+import lesson17 from './class/lesson17';
+console.log(lesson17.A);
